a49cd835951d8b8332e5ee6f6022d3341df287a3
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2018-09-14 13:33:55 +0200

    Mejora de código

--- PruebaGIT/src/net/eguiluz/PruebaParaGit.java
+++ PruebaGIT/src/net/eguiluz/PruebaParaGit.java
@@ -4,6 +4,7 @@ public class PruebaParaGit {
 
 	public static void main(String[] args) {
 		System.out.println( "Quiero probar a sincronizar esto" );
+		System.out.println( "Y a ver si se hace otro cambio");
 	}
 
 }
